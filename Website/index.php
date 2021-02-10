5e0634315d1cb8b0bb8a01c270a86a9d1eddc367
B-Norbert <73950450+B-Norbert@users.noreply.github.com>
2021-02-10 14:16:46 +0000

    test1

---
+++ Website/index.php
@@ -0,0 +1,75 @@
+<?php include("data/db.php");?>
+<?php
+?>
+<?php include("navbar.php");?>
+<div class='footer-container'>
+        <div class='footer-links'>
+          <div class='footer-link-wrapper'>
+            <div class='footer-link-items'>
+              <h2>Helpful Quicklines</h2>
+              <a href='https://www.confused.com/'>Insurance</a>
+              <a href='../../search.php'>Search</a>
+              <a href='../../contact.php'>Contact</a>
+            </div>
+            <div class='footer-link-items'>
+              <h2 size="14px" color="#ffc107">Connect with us</h2>
+              <a href='/'>Instagram</a>
+              <a href='/'>Facebook</a>
+              <a href='/'>Youtube</a>
+              <a href='/'>Twitter</a>
+            </div>
+          </div>
+        </div>
+        <section class='social-media'>
+          <div class='social-media-wrap'>
+            <small class='website-rights'>
+  
+  (Company to edit). All rights reserved.
+  
+  </small>
+            <div class='social-icons'>
+              <a
+                class='social-icon-link facebook'
+                href='/'
+                target='_blank'
+                aria-label='Facebook'
+              >
+                <i class='fab fa-facebook-f' />
+              </a>
+              <a
+                class='social-icon-link instagram'
+                href='/'
+                target='_blank'
+                aria-label='Instagram'
+              >
+                <i class='fab fa-instagram' />
+              </a>
+              <a
+                class='social-icon-link youtube'
+                href='/'
+                target='_blank'
+                aria-label='Youtube'
+              >
+                <i class='fab fa-youtube' />
+              </a>
+              <a
+                class='social-icon-link twitter'
+                href='/'
+                target='_blank'
+                aria-label='Twitter'
+              >
+                <i class='fab fa-twitter' />
+              </a>
+              <a
+                class='social-icon-link twitter'
+                href='/'
+                target='_blank'
+                aria-label='LinkedIn'
+              >
+                <i class='fab fa-linkedin' />
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+</body>
